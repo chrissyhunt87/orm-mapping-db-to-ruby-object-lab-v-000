d2c16b19310106647f525453fe5cb7e4345cd8f4
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-12-02 16:36:38 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -75,9 +75,7 @@ class Student
       FROM students
       WHERE grade = ?
     SQL
-    DB[:conn].execute(sql, grade).map do |row|
-      self.new_from_db(row)
-    end.first
+    DB[:conn].execute(sql, grade)
   end
 
   def self.all
