23179832bd9cb0c86f47b9fa31f4fb2f57e3e4fb
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-12-02 16:32:45 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,6 +47,15 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def self.first_X_students_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade < 12
+    SQL
+    DB[:conn].execute(sql)
+  end
+
   def self.all
     sql = <<-SQL
       SELECT *
